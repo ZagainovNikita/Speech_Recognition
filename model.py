6afb7070b35f6e352bfde378055e8b4d896185a1
NikitaZagainov <126257067+ZagainovNikita@users.noreply.github.com>
2023-12-25 11:43:53 +0300

    Update model.py

--- model.py
+++ model.py
@@ -2,13 +2,13 @@ from transformers import Wav2Vec2Processor, AutoModelForCTC
 
 def get_model(device="cpu"):
     return AutoModelForCTC.from_pretrained(
-        "jonatasgrosman/wav2vec2-xls-r-1b-russian", 
+        "YOUR_REPO_ID", 
         cache_dir="huggingface_models"
     ).to(device)
 
 
 def get_processor():
     return Wav2Vec2Processor.from_pretrained(
-        "jonatasgrosman/wav2vec2-xls-r-1b-russian",
+        "YOUR_REPO_ID",
         cache_dir="huggingface_models"
     )
